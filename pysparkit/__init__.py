d603fcc9be99844daa7ec1426b5bd2ba722057ce
innerNULL <lyt697@outlook.com>
2021-07-20 11:03:01 +0800

    [Dev] Adds map ops.

--- pysparkit/__init__.py
+++ pysparkit/__init__.py
@@ -5,20 +5,23 @@
 
 import os
 import pyspark.sql 
-from typing import Dict
+from typing import Dict, List
 
 
 def get_common_spark_session(
         job_name: str, spec_conf: Dict = {
             "hive.metastore.client.socket.timeout": 360, 
             "spark.ui.showConsoleProgress": "true", 
-        }) -> pyspark.sql.SparkSession:
+        }, py_files: List[str] = []) -> pyspark.sql.SparkSession:
     spark_conf = pyspark.sql.SparkSession.builder.appName(job_name)
     for k, v in spec_conf.items():
         spark_conf.config(k, v)
     spark_session: pyspark.sql.SparkSession = spark_conf\
             .enableHiveSupport()\
             .getOrCreate()
+
+    for file_ in py_files:
+        spark_session.sparkContext.addPyFile(file_)
     return spark_session
 
 
@@ -27,4 +30,7 @@ def python_version_config(version: str="python3") -> None:
         os.environ["PYSPARK_PYTHON"]="/usr/bin/python3"
         os.environ["PYSPARK_DRIVER_PYTHON"]="/usr/bin/python3"
 
+
+def get_pysparkit_root() -> str:
+    return os.path.abspath(__file__)
 
--- pysparkit/op/map_ops.py
+++ pysparkit/op/map_ops.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
-# file: converter.py
+# file: map_ops.py
 # date: 2021-07-16
 
 
+import pyspark
 import pyspark.sql
 from typing import Dict, List, Union
-from pyspark.sql import SparkSession
-from pyspark.sql import Row
-from pyspark import SparkFiles
-from pyspark import SparkContext
-from pyspark.sql.types import *
-from pyspark.sql.functions import rand
+
+
 
 
 def json_str2spark_row(json_str: str) -> pyspark.sql.Row:
     py_dict: Dict = json.loads(json_str) 
     output: pyspark.sql.Row = pyspark.sql.Row(**py_dict)
     return output
+
+
+def sparksql_row2py_dict(row: pyspark.sql.Row) -> Dict:
+    return row.asDict()
