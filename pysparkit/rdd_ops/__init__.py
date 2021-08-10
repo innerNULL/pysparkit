a53759a1ff844bc256311c8703784119e2092741
innerNULL <lyt697@outlook.com>
2021-08-10 14:57:01 +0800

    [Dev] Adds rdd ops.

--- pysparkit/rdd_ops/__init__.py
+++ pysparkit/rdd_ops/__init__.py
@@ -7,6 +7,7 @@ import pyspark
 from typing import Dict, List, Any, Union
 from ..lambda_ops import reduce_ops
 from ..lambda_ops import map_ops
+from ..incubator import helpy as hpy
 
 
 
@@ -32,3 +33,20 @@ def py_dict_rdd_distinct_by_field(
         py_dict_rdd: pyspark.RDD, distincting_keys: List
 ) -> pyspark.RDD:
     return None
+
+
+def py_dict_rdd_join_by_field(
+        py_dict_rdd_0: pyspark.RDD, py_dict_rdd_1: pyspark.RDD, 
+        target_field: Union[str, int, float], 
+        join_type: str="join"
+) -> pyspark.RDD:
+    pair_rdd_0: pyspark.RDD = py_dict_rdd_0\
+            .map(lambda x: (x[target_field], x))
+    pair_rdd_1: pyspark.RDD = py_dict_rdd_1\
+            .map(lambda x: (x[target_field], x))
+
+    joint_pair_rdd: pyspark.RDD = eval(
+            "pair_rdd_0.{join_type}(pair_rdd_1)".format(join_type=join_type))
+    joint_py_dict_rdd: pyspark.RDD = joint_pair_rdd\
+            .map(lambda x: hpy.iter.merge_dict(x[1][0], x[1][1], True))
+    return joint_py_dict_rdd
